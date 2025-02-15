9a48ec70435f44715932b001bb940ccea81aafba
Snowyegret <79635537+snowyegret23@users.noreply.github.com>
2025-02-15 15:52:07 +0900

    fix: path error, add:only chinese characters export
    - Fixed issue when non-ASCII strings are included in paths
    - Enable extraction of Chinese strings only

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text;
 using CsvHelper;
 using IniParser;
 using IniParser.Model;
@@ -31,7 +32,7 @@ class DllModifier
                     if (instruction.OpCode == OpCodes.Ldstr)
                     {
                         var text = instruction.Operand?.ToString() ?? string.Empty;
-                        if (string.IsNullOrEmpty(text))
+                        if (string.IsNullOrEmpty(text) || !ContainsChineseCharacters(text))
                             continue;
 
                         Console.WriteLine($"{type.Name} : {method.Name} : {text}");
@@ -50,6 +51,18 @@ class DllModifier
         using var writer = new StreamWriter(csvFilePath);
         using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
         csv.WriteRecords(entries);
+    }
+
+    static bool ContainsChineseCharacters(string text)
+    {
+        foreach (var c in text)
+        {
+            if (c >= 0x4E00 && c <= 0x9FFF) // CJK Unified Ideographs
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     static void ImportAndModifyAssembly(AssemblyDefinition assembly, string csvFilePath)
@@ -108,10 +121,10 @@ class DllModifier
         }
 
         var parser = new FileIniDataParser();
-        IniData configData = parser.ReadFile(iniPath);
+        IniData configData = parser.ReadFile(iniPath, fileEncoding:Encoding.UTF8);
 
         var targetDll = configData["Paths"]["TARGET_DLL"];
-        var editedDll = configData["Paths"]["EDITED_DLL"];
+        var backupDll = configData["Paths"]["BACKUP_DLL"];
         var csvFile = configData["Paths"]["CSV_FILE"];
         var assemblyDir = configData["Paths"]["ASSEMBLY_DIR"];
 
@@ -119,19 +132,24 @@ class DllModifier
         resolver.AddSearchDirectory(assemblyDir);
         var readerParameters = new ReaderParameters { AssemblyResolver = resolver };
 
-        using var assembly = AssemblyDefinition.ReadAssembly(targetDll, readerParameters);
-
         switch (mode)
         {
             case "-export":
             case "-e":
-                ExportLdstrStrings(assembly, csvFile);
+                using (var assembly = AssemblyDefinition.ReadAssembly(targetDll, readerParameters))
+                {
+                    ExportLdstrStrings(assembly, csvFile);
+                }
                 Console.WriteLine("CSV export가 완료되었습니다.");
                 break;
             case "-import":
             case "-i":
-                ImportAndModifyAssembly(assembly, csvFile);
-                assembly.Write(editedDll);
+                File.Copy(targetDll, backupDll, overwrite: true);
+                using (var assembly = AssemblyDefinition.ReadAssembly(backupDll, readerParameters))
+                {
+                    ImportAndModifyAssembly(assembly, csvFile);
+                    assembly.Write(targetDll);
+                }
                 Console.WriteLine("CSV import 완료, 텍스트가 수정되었습니다!");
                 Console.WriteLine("수정된 DLL이 저장되었습니다.");
                 break;
